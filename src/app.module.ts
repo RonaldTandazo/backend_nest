f7f7a4a754e48d61a42adb085260e491ea02f80e
RonaldTandazo <edu09085@hotmail.com>
2025-05-14 16:02:11 -0500

    modulo de form para controllers y services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FormController } from './controllers/form.controller';
-import { FormService } from './services/form.service';
 import { databaseConfig } from './config/database';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FormEntity } from './entities/form/form.entity';
+import { FormModule } from './modules/form/form.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseConfig),
-    TypeOrmModule.forFeature([FormEntity])
+    FormModule
   ],
-  controllers: [AppController, FormController],
-  providers: [AppService, FormService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
